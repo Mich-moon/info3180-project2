76e6b3998ac6a3591f915c54a83c6eb97e78c238
Michaela Wright <75323745+Mich-moon@users.noreply.github.com>
2022-04-24 07:06:22 -0500

    profile

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,191 @@
+<template>
+    <div class="container-fluid d-flex flex-column">
+        <div class="row px-0 component mb-5 px-2">
+            <div class="col-4 mx-0 p-2">
+                <img class="card-img-top rounded-circle" src="{{ user.photo }}" />
+            </div>
+            <div class="col-8 mx-0 p-2">
+                <div>
+                    <h3>{{ user.name }}</h3>
+                    <h5 class="grey">&#64;{{ user.username }}</h5>
+                    <p class="py-4 grey">{{ user.biography }}</p>
+                    <p><span class="grey">Email: </span><span class="fw-bold"> {{ user.email }}</span></p>
+                    <p><span class="grey">Location: </span><span class="fw-bold"> {{ user.location }}</span></p>
+                    <p><span class="grey">Joined: </span><span class="fw-bold"> {{ user.date_joined }}</span></p> 
+                </div>
+            </div>
+        </div>
+
+        <div class="form-head">
+            <h1>Cars Favourited</h1>
+        </div>
+        <hr/>
+        <div id="results" :v-if="results" class="results scroller">
+            <CarCard :v-if="results" v-for= "c in results" :key= "c.id" 
+				:id= "c.id"  
+                :photo = "c.photo"
+			    :make = "c.make"
+			    :model = "c.model"
+			    :year = "c.year" 
+			    :price = "c.price"
+        	/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import CarCard from '@/components/CarCard.vue'
+
+    export default {
+        components: { CarCard },
+        data() {
+            return {
+                csrf_token: "",
+                logged_in: false,
+                id: null,
+                user: {
+                    id: 0,
+                    photo: "",
+                    name: "Patricia Clark",
+                    username: "dpatrick",
+                    biography: "someting here",
+                    email: "p@yahoo.com",
+                    location: "Jamaica",
+                    date_joined: "January",
+                },
+                results: [{
+                    'id': 0,
+                    'photo': '', 
+                    'year': 2019, 
+                    'make': 'Honda', 
+                    'model': 'Civic', 
+                    'price': 7000.00
+                }]
+                //results: []
+            };
+        },
+        created() {
+            this.isLoggedIn();
+            this.getCsrfToken();
+            this.getUser();
+            this.getFavourites();
+        },
+        methods: {
+            isLoggedIn() {
+                let self = this;
+
+                if (localStorage.getItem("token")) {
+                    self.logged_in = true;
+
+                    // retrieve user id from token stored in localstorage
+                    let jwt_token = localStorage.getItem("token");
+
+                    var base64Url = jwt_token.split('.')[1];
+                    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+                    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+                        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+                    }).join(''));
+
+                    let jwt_payload = JSON.parse(jsonPayload);
+                    self.id = jwt_payload['sub'];
+
+                } else {
+                    self.logged_in = false;
+                    // redirect to home page
+                    self.$router.push({ path: '/'});
+                }
+            },
+            getUser() {
+                let self = this;
+
+                fetch("/api/users/" + this.user_id, {
+                    headers: {
+                        Authorization: "Bearer " + localStorage.getItem("token"),
+                        Accept: "application/json",
+                    },
+                })
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+
+                    if ("errors" in data) {
+                        // redirect to home page
+                        this.$router.push({ path: "/" });
+                    } else {
+                        self.user = data.user;
+                    }
+                });
+            },
+            getFavourites(){
+                let self = this;
+
+                fetch("/api/users/" + self.id + "/favourites", {
+                    headers: {
+                        "Authorization" : "Bearer " + localStorage.getItem("token"),
+                        "Accept" : "application/json"
+                    }
+                })
+                .then(function (response) {
+                    console.log(response);
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+
+                    this.results = JSON.parse(data.results);
+                })
+                .catch(function (error) {
+                    self.results = [error];
+                    console.log(error);
+                });
+            },
+            getCsrfToken() {
+                let self = this;
+
+                fetch("/api/csrf-token")
+                .then((response) => response.json())
+                .then((data) => {
+                console.log(data);
+                self.csrf_token = data.csrf_token;
+                })
+            },
+        },
+    };
+</script>
+
+<style scoped>
+.component {
+    margin-top: 50px;
+    width: 65%;
+}
+.form-head {
+    width: 70%;
+    margin-bottom: 0px;;
+}
+.results {
+    width:70%;
+    margin: 0 auto;
+    padding: 30px;
+}
+.scroller {
+    overflow-y: scroll;
+    scrollbar-color: rebeccapurple green;
+    scrollbar-width: thin;
+}
+.msg__list {
+    list-style: none;
+}
+.msg__item {
+    display: inline-block;
+    vertical-align: top;
+}
+.grey{
+  color: grey;
+}
+hr {
+    width: 75%;
+    margin: 20px auto;
+}
+</style>
